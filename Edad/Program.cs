d8ae098de40e36edb5f17238111096afa7a25691
Aileen Giraldo <aileensofia2008@gmail.com>
2025-08-13 14:41:48 -0500

    Ejercicio de edad

---
+++ Edad/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Edad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 0;
+            Console.WriteLine("Por favor ingrese su edad");
+            edad = Convert.ToInt32(Console.ReadLine());
+
+            if (edad >=18)
+            { 
+                Console.WriteLine("Bienvenido a mi sitio web");
+            }
+            else
+            {
+                Console.WriteLine("No puedes entrar a este sitio web");
+            }
+        }
+    }
+}
