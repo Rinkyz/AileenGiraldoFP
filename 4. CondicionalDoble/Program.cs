28b37a359bbe42d06052d4ac5be33a78624ca05e
Aileen Giraldo <aileensofia2008@gmail.com>
2025-08-13 14:58:11 -0500

    Condicionales dobles

---
+++ 4. CondicionalDoble/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _4._CondicionalDoble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Crear un algoritmo que permita ingresar el nombre y el sueldo de una persona, si el sueldo supera los 3000 pesos
+            mostrar el mensaje en pantalla con el nombre de la persona, indicando que debe abonar impuestos. 
+            De lo contrario, mostrar el mensaje "Persona exenta de impuestos". */
+
+            string nombre = "";
+            int sueldo = 0;
+
+            Console.WriteLine("ingrese el nombre");
+            nombre = Console.ReadLine(); 
+
+            Console.WriteLine(nombre);
+            Console.WriteLine("Ingrese el sueldo");
+
+            sueldo = Int32.Parse(Console.ReadLine());
+
+            if (sueldo >= 3000)
+            {
+                //Verdadera
+                Console.WriteLine($"La persona {nombre}, debe abonar impuestos");
+            }
+            else
+            {
+                //Falsa
+                Console.WriteLine($"La persona {nombre}, no debe abonar impuestos");
+            }
+        }
+    }
+}
