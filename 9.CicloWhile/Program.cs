72e54453a4a061570413800d56673977d88bffc6
Aileen Giraldo <aileensofia2008@gmail.com>
2025-09-03 14:09:33 -0500

    Ciclowhile

---
+++ 9.CicloWhile/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _9.CicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear un algoritmo que permita calcular la factorial de un número dado ppor el usuario.
+
+            int contador = 0;
+            int acumulador = 0:
+
+            while (contador <= 5)
+            {
+                acumulador += contador; 
+                Console.WriteLine($"C: {contador} - A: {acumulador}");
+                contador++;
+            }
+            Console.WriteLine($"La cuma de los 5 primeros números enteros es: {acumulador}");
+        }
+    }
+}
