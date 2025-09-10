a6d786329bcdb1199a63e2f57cfad8e533fa466c
Aileen Giraldo <aileensofia2008@gmail.com>
2025-09-10 15:06:00 -0500

    DoWhile3

---
+++ 19.DoWhile3/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _19.DoWhile3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 0;
+            string género = "";
+            int contadormujer = 0;
+            int contadorhombre = 0;
+            int contadormayor = 0;
+            int contadormenor = 0;
+            int estudiantes = 0;
+
+            do
+            {
+                estudiantes++;
+
+                Console.WriteLine("Ingrese su edad");
+                edad = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Ingrese su género. m:mujer, h:hombre");
+                género = Console.ReadLine().ToLower();
+
+                if (edad >= 18)
+                {
+                    contadormayor++;
+                }
+                else
+                {
+                    contadormenor++;
+                }
+
+                if (género == "m")
+                {
+                    contadormujer++;
+                }
+                if (género == "h")
+                {
+                    contadorhombre++;
+                }
+
+            } while (estudiantes < 5);
+
+            Console.WriteLine($"Mayores de edad: {contadormayor}, Menores de edad: {contadormenor}, Hombres: {contadorhombre}, Mujeres: {contadormujer}");
+        }
+    }
+}
