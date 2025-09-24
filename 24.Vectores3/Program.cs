4e8b7774eced991fb95c952fe77dc2f90060658d
Aileen Giraldo <aileensofia2008@gmail.com>
2025-09-24 15:21:53 -0500

    Ejercicios de vectores y taller vectores

---
+++ 24.Vectores3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _24.Vectores3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[10];
+            float acumulador = 0;
+            float promedio = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Ingrese el dato para la posición {i + 1}, con índice {i}");
+                numeros[i] = int.Parse(Console.ReadLine());
+                acumulador += numeros[i];
+
+                Console.WriteLine($"P: {i + 1} -I: {i}: {numeros[i]}");
+            }
+
+            promedio = acumulador / 10;
+
+            Console.WriteLine($"el promedio de los números ingresados en el arreglo es: {promedio}");
+        }
+    }
+}
---
+++ 25.TallerVectores/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Drawing;
+using System.Net;
+
+namespace _25.TallerVectores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1. Escribir un algoritmo que permita llenar un vector[15] con números enteros, y luego 
+              encuentre y muestre el valor máximo y mínimo de los números ingresados.
+
+            int[] numeros = new int[5];
+
+            int mayor;
+            int menor;
+
+            mayor = numeros[0];
+            menor = numeros[0];
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Ingrese el dato para la posición {i + 1}, con índice {i}");
+                numeros[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine($"P: {i + 1} -I: {i}: {numeros[i]}");
+
+                if (numeros[i] > mayor)
+                {
+                    mayor = numeros[i];
+                }
+                if (numeros[i] < menor)
+                {
+                    menor = numeros[i];
+                }
+            }
+
+            Console.WriteLine($"El valor máximo es: {mayor} - El valor mínimo es: {menor}");
+
+            2. Escribe un algoritmo que permita ingresar caracteres en un vector, y luego invierta el
+            orden de los elementos del vector.Se deben mostrar lo dos vectores. */
+
+            char[] caracteres = new char[9];
+
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine($"Ingrese el dato para la posición {i + 1}, con índice {i}");
+                caracteres[i] = char.Parse(Console.ReadLine());
+
+                Console.WriteLine($"P: {i + 1} -I: {i}: {caracteres[i]}");
+            }
+
+
+        }
+    }
+}
