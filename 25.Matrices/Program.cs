36d0f0e7dfb80c26f7cfef5a7be44cc3f482796e
LauraArdila08 <laura.ardilaa@upb.edu.co>
2025-09-26 15:30:21 -0500

    Matrices y ejercicio 26

---
+++ 25.Matrices/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace _25.Matrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Arreglos bidimensionales - Matrices
+            int[,] numeros = new int[2, 2]; //rango=fxc=2x2=4
+                        
+            bool[,] banderas = new bool[2, 5];
+            object[,] objetos = new object[2, 5];
+
+            //Asignación de valores con f,c
+
+            numeros[0, 0] = 15;
+            numeros[0, 1] = 25;
+            numeros[1, 0] = 32;
+            numeros[1, 1] = 4;
+            //numeros[1, 2] = 7; Error por índices fuera de los rangos
+            //Recuperar datos almacenados en una posición 
+
+            Console.WriteLine($"Valor almacenado en la posición con índices 1, 0: {numeros[1, 0]}");
+
+            //Declaración e inicialización de matrices con valores por defecto
+
+            string[,] apellidos =
+                {
+                    {"Chica", "López", "Álvarez"},
+                    {"Patiño", "Ciro", "Botero" },
+                    {"Duque", "Carmona", "Castaño"}
+                };
+
+            Recorrer para almacenar datos
+
+            string[,] nombres = new string[3, 2];
+
+            for (int i = 0; i < 3; i++)//Recorrer las filas
+            {
+                for(int j = 0; j < 2; j++)//Recorrer las columnas
+                {
+                    Console.WriteLine($"Digite el nombre para almacenarlo en la posición con índices {i}, {j}");
+                    nombres[i, j] = Console.ReadLine();
+                    
+                }
+            }
+
+            //Recorrer para recuperar los datos
+
+            for(int i = 0;i < nombres.GetLength(0); i++)//.GetLength(0) devuelve el número de filas
+            {
+                for (int j = 0;j < nombres.GetLength(1); j++)//.GetLength devuelve el número de columnas
+                {
+                    Console.WriteLine($"Nombre almacenado en la posición {i}, {j}: {nombres[i,j]}");
+                
+                }
+            }
+
+            for (int i = 0; i < nombres.GetLength(0); i++)
+            {
+                for (int j = 0; j < nombres.GetLength(1); j++)
+                {
+                    Console.Write($"{nombres[i, j]}     |");
+                }
+                Console.WriteLine();
+            }*/
+
+            char[,] caracteres = new char[2, 4];
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.WriteLine($"Digite el caracter para almacenarlo en la posición con índices {i}, {j}");
+                    caracteres[i, j] = char.Parse(Console.ReadLine());
+
+                }
+            }
+            for (int i = 0; i < caracteres.GetLength(0); i++)
+            {
+                for (int j = 0; j < caracteres.GetLength(1); j++)
+                {
+                    Console.WriteLine($"Nombre almacenado en la posición {i}, {j}: {caracteres[i, j]}");
+                }
+            }
+            for (int i = 0; i < caracteres.GetLength(0); i++)
+            {
+                for (int j = 0; j < caracteres.GetLength(1); j++)
+                {
+                    Console.Write($"{caracteres[i, j]}     |");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 26.Matrices2/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace _26.Matrices2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escribe un programa que realice la suma de dos matrices de dimensiones 2x3. 
+              Requisitos del programa:
+
+              Solicita al usuario que ingrese los elementos de la primera matriz de 2 filas y 3 columnas. 
+              Solicita al usuario que ingrese los elementos de la segunda matriz de las mismas dimensiones (2x3). 
+              Calcula la matriz suma, resultado de sumar cada elemento correspondiente de las dos matrices. 
+              Muestra la matriz resultante de la suma en formato de matriz (2 filas, 3 columnas). */
+
+            int[,] numeros1 = new int[2, 3];
+            int[,] numeros2 = new int[2, 3];
+            int[,] suma = new int[2, 3];
+
+            int acumulador = 0;
+
+            //Ingresa los numeros en ambas matrices
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine($"Digite el número para almacenarlo en la posición con índices {i}, {j} de la primera matriz");
+                    numeros1[i, j] = Int32.Parse(Console.ReadLine());
+                } 
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine($"Digite el número para almacenarlo en la posición con índices {i}, {j} de la segunda matriz");
+                    numeros2[i, j] = Int32.Parse(Console.ReadLine());
+                }
+            }
+
+            //Se muestran los datos de cada matriz
+            //Matriz 1
+            for (int i = 0; i < numeros1.GetLength(0); i++)
+            {
+                for (int j = 0; j < numeros1.GetLength(1); j++)
+                {
+                    Console.WriteLine($"El número almacenado en la posición {i}, {j}: {numeros1[i, j]}");
+                }
+            }
+            for (int i = 0; i < numeros1.GetLength(0); i++)
+            {
+                for (int j = 0; j < numeros1.GetLength(1); j++)
+                {
+                    Console.Write($"{numeros1[i, j]}     |");
+                }
+                Console.WriteLine();
+            }
+            //Matriz 2
+            for (int i = 0; i < numeros2.GetLength(0); i++)
+            {
+                for (int j = 0; j < numeros2.GetLength(1); j++)
+                {
+                    Console.WriteLine($"El número almacenado en la posición {i}, {j}: {numeros2[i, j]}");
+                }
+            }
+            for (int i = 0; i < numeros2.GetLength(0); i++)
+            {
+                for (int j = 0; j < numeros2.GetLength(1); j++)
+                {
+                    Console.Write($"{numeros2[i, j]}     |");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("La suma de ambas matrices es:");
+
+            for (int i = 0; i < suma.GetLength(0); i++)
+            {
+                for (int j = 0; j < suma.GetLength(1); j++)
+                {
+                    Console.Write($"{numeros1[i,j] + numeros2[i,j]}     |");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
