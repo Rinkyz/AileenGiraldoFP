0880f140926f8eb4f2e9d6c3dc67d224f41febbd
Aileen Giraldo <aileensofia2008@gmail.com>
2025-10-15 14:22:17 -0500

    Ejercicio 28. (No me guardó en clase)

---
+++ 28.ProgramaciónModular/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _28.ProgramaciónModular
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MostrarMensaje();
+            MostrarMensajes("Soy Aileen");
+            MostrarMensajes("Tengo 17 años");
+            MostrarMensajes($"Tengo {CalcularEdad()} años");
+            MostrarMensajes($"Tengo {CalcularEdad(2025, 2008)} años");
+        }
+        //Módulo 1 - Procedimiento sin parámetros
+        static void MostrarMensaje()
+        {
+            Console.WriteLine("Bienvenido a la programación modular");
+            MostrarMensajes("Estoy siendo llamado desde MostrarMensajes()");
+        }
+        //Módulo 2 - Procedimiento con parámetros
+        static void MostrarMensajes(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+        }
+        //Módulo 3 - Función sin parámetros
+
+        static int CalcularEdad()
+        {
+            int edad = 2025 - 2008;
+            return edad;
+        }
+
+        //Módulo 4 - Función con parámetros
+        static int CalcularEdad(int añoActual, int añoNacimiento)
+        {
+            return añoActual - añoNacimiento;
+        }
+    }
+}
